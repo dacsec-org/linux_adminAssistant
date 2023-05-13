35a9cce9b12b965a7a271283aa327d9d8890ecc2
aaronms1 <amsanon1@protonmail.com>
2023-05-13 10:29:01 -0400

    wrote OS_detect.pl_script

---
+++ src/install_scripts/os_detect.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+# This script's sole purpose is for Operating system detection for the purpose of installing the correct packages
+# for the correct OS.
+# run perl os_detect.pl from its directory to see the output(you may have to use sudo)
+use strict;
+use warnings;
+use warnings FATAL => 'all';
+
+my $os = `uname -s`;
+chomp($os);
+if ($os eq "Linux") {
+    my $distro = `lsb_release -si`;
+    chomp($distro);
+    if ($distro eq "Ubuntu") {
+        print "Ubuntu\n";
+    } elsif ($distro eq "Debian") {
+        print "Debian\n";
+    } elsif ($distro eq "Fedora") {
+        print "Fedora\n";
+    } elsif ($distro eq "RedHatEnterpriseServer") {
+        print "RedHat\n";
+    } elsif ($distro eq "Parrot") {
+        print "Parrot\n";
+    } elsif ($distro eq "Kali") {
+        print "Kali\n";
+    } elsif ($distro eq "CentOS") {
+        print "CentOS\n";
+    } else {
+        print "Unknown Linux Distro\n";
+    }
+
+} elsif ($os eq "Darwin") {
+    print "MacOS\n";
+    } else {
+    print "Unknown OS\n";
+}
